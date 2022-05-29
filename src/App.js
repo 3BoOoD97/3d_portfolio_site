e0f13c8a68d6419b22cc85d48ff27972d02d3ba5
3BoOoD97 <abodn70@hotmail.com>
2022-05-29 18:26:09 +0200

    Fix NavBar & TimeLine Done+ About section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Spline from '@splinetool/react-spline';
 import './App.css';
 import {IoMenu} from 'react-icons/io5';
 import {useState} from 'react';
+import me from './img/me.jpg';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { Experience} from "./data";
 
 function App() {
   const [isActive, setIsActive] = React.useState(false);
   return (
 
-       <div className='flex w-screen h-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20'>
+       <div className='flex w-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20'>
        
         {/*NAV BAR*/}
     <nav className='w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center'>
@@ -71,7 +75,60 @@ function App() {
   </div>
 </div>
         </div>
-        {/* Main Secation */}
+        {/* Main section */}
+        <main className='w-[80%] mt-5'>
+          {/* About */}
+          <section className='w-full grid grid-cols-1 md:grid-cols-2 gap-4 my-24' id='about'>
+         {/*Image Box*/}
+            <div className='w-full h-420 flex items-center justify-center'>
+              <div className='w-275 h-340 bg-emerald-200 rounded-md relative'>
+                <img className='w-full h-full absolute -right-4 top-4 object-cover
+                rounded-lg shadow-lg' src={me} alt=''/>
+              </div>
+            </div>
+
+{/* Content Box*/}
+            <div className='w-full h-420 flex flex-col items-center justify-center'>
+              <p className='text-lg text-textBase text-center'>Hi I am Abod</p>
+              <button class="w-full md:w-auto relative mt-6 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800 hover:shadow-lg hover:shadow-teal-500/50 hover:dark:shadow-lg hover:dark:shadow-teal-800/80">
+                <span class="w-full md:w-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                  Download
+                </span>
+              </button>
+            </div>
+            </section>
+
+{/* TimeLine */}
+<section className="w-full flex items-center justify-center">
+  <VerticalTimeline>
+              {Experience &&
+                Experience.map((n) => (
+                  <VerticalTimelineElement
+                    key={n.id}
+                    className="vertical-timeline-element--work"
+                    contentStyle={{
+                      background: "rgb(21, 24, 31)",
+                      color: "#888",
+                    }}
+                    contentArrowStyle={{
+                      borderRight: "7px solid  rgb(21, 24, 31)",
+                    }}
+                    date={n.date}
+                    iconStyle={{ background: "rgb(21, 24, 31)", color: "#888" }}
+                    icon={n.iconsSrc}
+                  >
+                    <h3 className="vertical-timeline-element-title">
+                      {n.title}
+                    </h3>
+                    <h4 className="vertical-timeline-element-subtitle">
+                      {n.location}
+                    </h4>
+                    <p>{n.description}</p>
+                  </VerticalTimelineElement>
+                ))}
+            </VerticalTimeline>
+          </section>
+        </main>
     </div>
  
   );
