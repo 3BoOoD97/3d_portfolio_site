9d8b50436e69cfbbf9a8a043a862ec51703feead
3BoOoD97 <abodn70@hotmail.com>
2022-06-04 23:35:21 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -12,42 +12,54 @@ import { AnimatePresence, motion } from "framer-motion";
 import { type } from '@testing-library/user-event/dist/type';
 
 function App() {
-  const [isActive, setIsActive] = React.useState(false);
+  const [isActive, setIsActive] = useState(false);
   return (
     <AnimatePresence initial={false}>
+      <div className="flex w-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
+        <nav className="w-full px-6 z-50  fixed inset-x-0 top-2 flex justify-center items-center ">
+          <div className=" w-full md:w-880 bg-navBar p-4 rounded-2xl flex items-center">
+            <p className="text-lg text-slate-200 font-medium">ABOD</p>
 
-       <div className='flex w-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20'>
-       
-        {/*NAV BAR*/}
-    <nav className='w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center'>
-      <div className='w-full md:w-880 bg-navBar p-4 rounded-2xl flex items-center'>
-      <p className='text-lg text-slate-200 font-medium'> Abod</p> 
-      <div className='hidden md:flex items-center gap-6 ml-6 flex-1'>
-        <a href="#home" className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 
-        ease-in-out">Home</a>
+            <div className="hidden md:flex items-center gap-6 ml-6 flex-1">
+              <a
+                href="#home"
+                className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+              >
+                Home
+              </a>
+              <a
+                href="#about"
+                className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+              >
+                About
+              </a>
+              <a
+                href="#projects"
+                className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+              >
+                Projects
+              </a>
+              <a
+                href="#contacts"
+                className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+              >
+                Contact
+              </a>
+              <a
+                href="#"
+                className="ml-auto text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in"
+              >
+                Download
+              </a>
+            </div>
 
-<a href="#about" className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 
-        ease-in-out">About</a>
-
-<a href="#projects" className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 
-        ease-in-out">Projects</a>
-
-<a href="#contact" className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 
-        ease-in-out">Contact</a>
-
-        <a href='#' className='ml-auto text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in-out'>
-      Download
-      </a>
-      </div>
-    
-      <motion.div
+            <motion.div
               whileTap={{ scale: 0.6 }}
               className="block md:hidden  ml-auto cursor-pointer"
               onClick={() => setIsActive(!isActive)}
             >
               <IoMenu className="text-2xl text-textBase " />
             </motion.div>
-
             {isActive && (
               <motion.div
                 initial={{ opacity: 0, scale: 0.5 }}
@@ -56,66 +68,91 @@ function App() {
                 transition={{ delay: "0.1s", type: "spring" }}
                 className="p-4 w-275 bg-navBar rounded-lg fixed top-24 right-16 flex flex-col items-center justify-evenly gap-6"
               >
-    
-<a href="#home" className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 
-        ease-in-out"
-        onClick={()=>setIsActive(false)}>Home</a>
+                <a
+                  href="#home"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+                  onClick={() => setIsActive(false)}
+                >
+                  Home
+                </a>
+                <a
+                  href="#about"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+                  onClick={() => setIsActive(false)}
+                >
+                  About
+                </a>
+                <a
+                  href="#projects"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+                  onClick={() => setIsActive(false)}
+                >
+                  Projects
+                </a>
+                <a
+                  href="#contacts"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in"
+                  onClick={() => setIsActive(false)}
+                >
+                  Contact
+                </a>
+                <a
+                  href="#"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in"
+                  onClick={() => setIsActive(false)}
+                >
+                  Download
+                </a>
+              </motion.div>
+            )}
+          </div>
+        </nav>
 
-<a href="#about" className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 
-        ease-in-out"
-        onClick={()=>setIsActive(false)}>About</a>
-
-<a href="#projects" className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 
-        ease-in-out"
-        onClick={()=>setIsActive(false)}>Projects</a>
-
-<a href="#contact" className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 
-        ease-in-out"
-        onClick={()=>setIsActive(false)}>Contact</a>
-
-        <a href='#' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase 
-        px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in-out'
-        onClick={()=>setIsActive(false)}>
-      Download
-      </a>
-      </motion.div>      
-      )}
-      </div>
-      </nav>
-      <div className='relative' id='home'>
-      <Spline scene="https://prod.spline.design/0TUuMfCgXS3Xkkuo/scene.splinecode" />
-<div className='absolute bottom-10 w-full justify-center items-center flex'>
-  <div className='shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl '>
-    <p className='text-white'> Press and darg</p>
-  </div>
-</div>
+        <div className="relative" id="home">
+        <Spline scene="https://prod.spline.design/0TUuMfCgXS3Xkkuo/scene.splinecode" />
+          <div className="absolute bottom-10 w-full flex justify-center items-center">
+            <div className="shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl ">
+              <p className="text-white">Press and drag to orbit</p>
+            </div>
+          </div>
         </div>
-        {/* Main section */}
-        <main className='w-[80%] mt-5'>
-          {/* About */}
-          <section className='w-full grid grid-cols-1 md:grid-cols-2 gap-4 my-24' id='about'>
-         {/*Image Box*/}
-            <div className='w-full h-420 flex items-center justify-center'>
-              <div className='w-275 h-340 bg-emerald-200 rounded-md relative'>
-                <img className='w-full h-full absolute -right-4 top-4 object-cover
-                rounded-lg shadow-lg' src={me} alt=''/>
+
+        <main className="w-[80%] mt-4">
+          <section
+            className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 my-24"
+            id="about"
+          >
+            <div className="w-full h-420 flex items-center justify-center">
+              <div className="w-275 h-340 relative bg-emerald-200 rounded-md">
+                <img
+                  src={me}
+                  alt=""
+                  className="w-full h-full absolute -right-4 top-4 object-cover rounded-lg drop-shadow-2xl"
+                />
               </div>
             </div>
+            <div className="w-full h-420 flex flex-col items-center justify-center ">
+              <p className="text-lg text-textBase text-center">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Sapiente dicta recusandae commodi praesentium officia, aliquam
+                cumque voluptatem dolor enim ducimus id maiores consequatur
+                dolorem! Architecto aperiam corrupti nostrum eum similique!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis saepe dolor illo. Tenetur labore impedit nihil natus
+                explicabo perspiciatis nisi autem aut numquam dolorum, voluptas
+                vero nobis assumenda commodi magni.
+              </p>
 
-{/* Content Box*/}
-            <div className='w-full h-420 flex flex-col items-center justify-center'>
-              <p className='text-lg text-textBase text-center'>Hi I am Abod</p>
-              <motion.button whileTap={{scale:0.8}} class="w-full md:w-auto relative mt-6 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800 hover:shadow-lg hover:shadow-teal-500/50 hover:dark:shadow-lg hover:dark:shadow-teal-800/80">
+              <button class="w-full md:w-auto relative mt-6 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800 hover:shadow-lg hover:shadow-teal-500/50 hover:dark:shadow-lg hover:dark:shadow-teal-800/80">
                 <span class="w-full md:w-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                   Download
                 </span>
-              </motion.button>
+              </button>
             </div>
-            </section>
+          </section>
 
-{/* TimeLine */}
-<section className="w-full flex items-center justify-center">
-  <VerticalTimeline>
+          <section className="w-full flex items-center justify-center">
+            <VerticalTimeline>
               {Experience &&
                 Experience.map((n) => (
                   <VerticalTimelineElement
@@ -144,39 +181,50 @@ function App() {
             </VerticalTimeline>
           </section>
 
-{/* Projects Section*/}
-          <section className='flex flex-wrap items-center justify-evenly my-24 gap-4' id='projects'>
-            {Projects && Projects.map(n => (
+          <section
+            className="flex flex-wrap items-center justify-evenly my-24 gap-4"
+            id="projects"
+          >
+            {Projects &&
+              Projects.map((n, i) => (
+                <motion.div
+                  key={n.id}
+                  className="border border-zinc-800 rounded-md p-2 min-w-[275px] md:max-w-[275px] hover:border-zinc-600 duration-100 ease-in-out"
+                >
+                  <p className="text-lg text-textBase font-medium uppercase">
+                    {n.name.length > 25 ? `${n.name.slice(0, 25)}...` : n.name}
+                  </p>
 
-            <div key={n.id} className='border border-zinc-800 rounded-md p-2 min-w-[275px] md:max-w-[275px]
- hover:border-zinc-600 duration-100 ease-out'>
-   <p className='text-md text-textBase font-medium uppercase'>
-     {n.name.length>20 ? `${n.name.slice(0,25)}...`: n.name}
-   </p>
-   <img src={'n.imgSrc'} className='w-full h-full object-cover rounded-md my-4' alt='' />
-   <div className=' flex flex-name justify-between items-center'>
-     <p className='text-lg text-gray-300'> Techno
-     <span className='block text-sm text-gray-500'>
-      {n.techs}
-    </span>
-     </p>
+                  <img
+                    src={n.imageSrc}
+                    className="w-full h-full object-cover rounded-md my-4"
+                    alt=""
+                  />
 
-     <a href={n.github}>
-       <motion.div whileTap={{scale:0.8}}>
-         <IoLogoGithub className='text-textBase text-3xl cursor-pointer' />
-       </motion.div>
-     </a>
-   </div>
- </div>
-
-))}
-
+                  <div className="flex flex-1 items-center justify-between">
+                    <p className="text-lg text-gray-300">
+                      Technologies
+                      <span className="block text-sm text-gray-500">
+                        {n.techs}
+                      </span>
+                    </p>
+                    <a href={n.github}>
+                      <motion.div whileTap={{ scale: 0.5 }}>
+                        <IoLogoGithub className="text-textBase text-3xl cursor-pointer" />
+                      </motion.div>
+                    </a>
+                  </div>
+                </motion.div>
+              ))}
           </section>
-          {/* Contact*/}
-          <section id='contact' className='flex flex-col items-center justify-center w-full my-24'>
-<p className='text-2xl text-gray-400 capitalize'>Follow me on</p>
-<div className='flex items-center justify-center w-full my-4 flex-wrap gap-4'>
-{SocialLinks &&
+
+          <section
+            id="contacts"
+            className="flex flex-col items-center justify-evenly w-full my-24"
+          >
+            <p className="text-2xl text-gray-400 capitalize">Follow me on</p>
+            <div className="flex items-center justify-evenly w-full my-4 flex-wrap gap-4">
+              {SocialLinks &&
                 SocialLinks.map((n) => (
                   <motion.a
                     whileTap={{ scale: 0.8 }}
@@ -187,15 +235,12 @@ function App() {
                     {n.iconSrc}
                     <p className="text-lg text-textBase">{n.name}</p>
                   </motion.a>
-
-))}
-
-</div>
-            </section>
+                ))}
+            </div>
+          </section>
         </main>
-    </div>
-
-</AnimatePresence>
+      </div>
+    </AnimatePresence>
   );
 }
 
